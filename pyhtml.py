10feb026901446487cc3b1a2473385f129dc0d59
Cenk Alti <cenkalti@gmail.com>
2017-05-09 19:51:43 +0300

    bump version

--- pyhtml.py
+++ pyhtml.py
@@ -193,7 +193,7 @@ from types import GeneratorType
 
 import six
 
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 
 # The list will be extended by register_all function.
 __all__ = 'Tag Block Safe Var SelfClosingTag html script style form'.split()
